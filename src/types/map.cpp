d9f517d3ebf651808f4830ee189b0b3cceff6878
Daniel Baker <bakerdude@gmail.com>
2023-11-23 19:39:01 +0100

    chore: delete comments

--- src/types/map.cpp
+++ src/types/map.cpp
@@ -86,36 +86,4 @@ void Map::render(tcod::Console& console) {
 
 void Map::set_highlight_tile(Vector2D position) { target_tile_ = position; }
 
-// void Map::save(TCODZip& zip) {
-//   for (int y = 0; y < get_height(); y++) {
-//     for (int x = 0; x < get_width(); x++) {
-//       zip.putInt(tiles_.at({x, y}).explored);
-//     }
-//   }
-// }
-// void Map::save(cereal::JSONOutputArchive& archive) {
-//   for (int y = 0; y < get_height(); y++) {
-//     for (int x = 0; x < get_width(); x++) {
-//       archive(tiles_.at({x, y}).explored);
-//     }
-//   }
-// }
-
-// void Map::load(TCODZip& zip) {
-//   for (int y = 0; y < get_height(); y++) {
-//     for (int x = 0; x < get_width(); x++) {
-//       bool explored = zip.getInt();
-//       tiles_.at({x, y}).explored = explored;
-//     }
-//   }
-// }
-// void Map::load(cereal::JSONInputArchive& archive) {
-//   for (int y = 0; y < get_height(); y++) {
-//     for (int x = 0; x < get_width(); x++) {
-//       bool explored;
-//       archive(explored);
-//       tiles_.at({x, y}).explored = explored;
-//     }
-//   }
-// }
 }  // namespace cpprl
