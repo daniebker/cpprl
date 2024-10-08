4b83202fcb1b274043fd7d50979ab87c12113e5a
Daniel Baker <bakerdude@gmail.com>
2024-10-08 20:11:59 +0100

    fix: include broken tests

--- tests/src/status_condition_systems_tests.cpp
+++ tests/src/status_condition_systems_tests.cpp
@@ -1,11 +1,32 @@
+#include "gtest/gtest.h"
+#include "components.hpp"
 
 /**
  * @file status_condition_systems_tests.cpp
  */
 
 /**
- * It should remove 1 hp from the player when afflicted with
- * a status condition that deals damage.
+ * Given an Entity
+ * And the entity has a StatusConditionComponent
+ * When the StatusConditionSystem is updated
+ * Then the entity should take damage from the status condition
  */
 
+TEST(StatusConditionSystem, TakeDamage) {
+  // Setup
+  auto entity = 1;
+  SupaRL::StatusConditionComponent statusConditionComponent {
+damege_per_turn_: 1,
+turns_remaining_: 1,
+turns_until_update_: 1,
+name_: "Bleed"
+  };
+  // Add the StatusConditionComponent to the entity
+  // AddEntityComponent(entity, statusConditionComponent);
 
+  // Update the StatusConditionSystem
+  // StatusConditionSystem::Update();
+
+  // Verify the entity has taken damage
+  // EXPECT_EQ(GetEntityHealth(entity), 99);
+}
